2a433cf41b004cf63f1e22b6f1c8d50a47778cff
Erik Mikael <erikfreecss@gmail.com>
2022-08-20 12:00:09 +0700

    update hotel cheapestPrice value

--- controller/hotel.js
+++ controller/hotel.js
@@ -47,7 +47,7 @@ export const getHotels = async (req, res, next) => {
   try {
     const hotels = await Hotel.find({
       ...others,
-      cheapestPrice: { $gt: min | 1000, $lt: max | 9999999 },
+      cheapestPrice: { $gt: min || 1, $lt: max || 9999 },
     }).limit(req.query.limit);
     res.status(200).json(hotels);
   } catch (err) {
